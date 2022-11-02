e95cff6e38ff069d057a02131dd072ab1de3e3f9
hjmunoz <hector.jefrey@gmail.com>
2022-11-02 12:06:42 -0500

    solucion de problemas librerias y server

--- ABCeducando-main/build/web/assets/cargaArchivos/carga.js
+++
@@ -1,9 +0,0 @@
-function cargarArchivo(elemento) {
-    var file = elemento.files[0];
-    var objHidden = document.formularioCargaMatricula.nombreArchivo;
-    objHidden.value = file.name;
-
-    document.formularioCargaMatricula.target = "null";
-    document.formularioCargaMatricula.action = "Usuario";
-    document.formularioCargaMatricula.submit();
-}
--- ABCeducando-main/src/java/controlador/Docentecontrolador.java
+++ ABCeducando-main/src/java/controlador/Docentecontrolador.java
@@ -45,6 +45,7 @@ public class Docentecontrolador extends HttpServlet {
 
         String opcion = request.getParameter("opcion");
         
+        
 
 
         
